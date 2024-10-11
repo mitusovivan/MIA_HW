8eec373ae25fc394d640ab5722dadf2cf265540a
mitusovivan <mitusovivan35@yandex.ru>
2024-10-11 19:17:48 +0300

    new file:   HomeWork(11.10.2024).cpp

---
+++ HomeWork(11.10.2024).cpp
@@ -0,0 +1,27 @@
+﻿#include <cmath>
+#include <iostream>
+
+int random_gen(int len){
+    int s, arr[len];
+    for (int i = 0; i < len; i++){
+        arr[i] = rand() % 10;
+        std::cout << ' ' << arr[i];
+        s += arr[i];
+    }
+    std::cout << '\n';
+    return s;
+}
+
+int main()
+{
+    std::cout << "Первая последовательность:";
+    int first_sum = random_gen(10);
+    std::cout << "Среднее первой последовательности = " << first_sum / 10.0 << '\n' << "Вторая последовательность:";
+    int second_sum = random_gen(10);
+    std::cout << "Среднее второй последовательности = " << second_sum / 10.0 << '\n' << "Третья последовательность:";
+    int fird_sum = random_gen(10);
+    std::cout << "Среднее третьей последовательности = " << fird_sum / 10.0 << '\n';
+    std::cin;
+    return 0;
+}
+
