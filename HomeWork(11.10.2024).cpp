1715fa4ef1bb76e03be4461d58e881e8a65dc559
mitusovivan <mitusovivan35@yandex.ru>
2024-10-16 19:10:08 +0300

    modified:   HomeWork(11.10.2024).cpp 	modified:   locator/locator.cpp

--- HomeWork(11.10.2024).cpp
+++ HomeWork(11.10.2024).cpp
@@ -1,26 +1,29 @@
-﻿#include <cmath>
+﻿#include <algorithm>//Подключение библиотек
+#include <cmath>
+#include <ctime>
 #include <iostream>
+#include <string>
 
-int random_gen(int len){
-    int s, arr[len];
-    for (int i = 0; i < len; i++){
-        arr[i] = rand() % 10;
-        std::cout << ' ' << arr[i];
-        s += arr[i];
-    }
-    std::cout << '\n';
-    return s;
-}
+void random_gen(int len, std::string NumberOfIterration, std::string NumberOfIterration2);//расположение функции генератора
 
 int main()
 {
-    std::cout << "Первая последовательность:";
-    int first_sum = random_gen(10);
-    std::cout << "Среднее первой последовательности = " << first_sum / 10.0 << '\n' << "Вторая последовательность:";
-    int second_sum = random_gen(10);
-    std::cout << "Среднее второй последовательности = " << second_sum / 10.0 << '\n' << "Третья последовательность:";
-    int fird_sum = random_gen(10);
-    std::cout << "Среднее третьей последовательности = " << fird_sum / 10.0 << '\n';
-    return 0;
+    srand(time(0));//Задание ключа генерации
+    random_gen(10, "Первая", "первой");//функции печати
+    random_gen(10, "Вторая", "второй");
+    random_gen(10, "Третья", "третьей");
+    return 0;//
 }
 
+void random_gen(int len, std::string NumberOfIterration, std::string NumberOfIterration2){//функция генератора
+    std::cout << NumberOfIterration << " последовательность:";//Вывод имени последовательности
+    int sum = 0;//сумма чисел
+    for (int i = 0; i < len; i++){//генерация чисел
+        int unit = (rand() % 10);
+        std::cout << ' ' << unit;
+        sum += unit;
+    }
+    float midow_arifmetic = float(sum) / len;//рассчёт ср.арифм
+    std::cout << '\n' <<"Среднее арифметическое "<< NumberOfIterration2; //вывод
+    std::cout <<" последовательности = " << midow_arifmetic << '\n';
+}
--- locator/locator.cpp
+++ locator/locator.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main(){
     int n, k;
+    //string s;
     cin >> n >> k;
     vector<int> arr(n, 0);
     for (int i = 0; i<n; i++) cin >> arr[i];
